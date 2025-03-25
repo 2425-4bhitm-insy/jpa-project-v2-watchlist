ef45dba4dc390242ae7abb41ea2508c8dcb5fda4
FlorianHayer <f.hayer@students.htl-leonding.ac.at>
2025-03-25 10:27:35 +0100

    feat: added Resources and added @ApplicationScoped to Repositories

--- src/main/java/at/htl/leonding/repository/MediaRepository.java
+++ src/main/java/at/htl/leonding/repository/MediaRepository.java
@@ -1,7 +1,9 @@
 package at.htl.leonding.repository;
 
 import at.htl.leonding.entities.Media;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class MediaRepository extends BetterRepository<Media> {
 	public MediaRepository() {
 		super(Media.class);
--- src/main/java/at/htl/leonding/repository/TagRepository.java
+++ src/main/java/at/htl/leonding/repository/TagRepository.java
@@ -1,7 +1,9 @@
 package at.htl.leonding.repository;
 
 import at.htl.leonding.entities.Tag;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class TagRepository extends BetterRepository<Tag> {
 	public TagRepository() {
 		super(Tag.class);
