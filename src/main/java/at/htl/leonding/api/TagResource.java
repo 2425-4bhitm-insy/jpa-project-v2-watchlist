acd763b5c7433e2e76f13d5994519a3eb311b63e
TimMayr <tim.mayr@gmx.at>
2025-04-29 08:43:58 +0200

    fix: delete

--- src/main/java/at/htl/leonding/api/TagResource.java
+++ src/main/java/at/htl/leonding/api/TagResource.java
@@ -1,5 +1,6 @@
 package at.htl.leonding.api;
 
+import at.htl.leonding.entities.Media;
 import at.htl.leonding.entities.Tag;
 import at.htl.leonding.repository.TagRepository;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -8,6 +9,8 @@ import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationScoped
 @Path("/tag")
@@ -39,7 +42,9 @@ public class TagResource {
     @Transactional
     @Produces(MediaType.APPLICATION_JSON)
     public Response delete(@PathParam("id") Long id) {
+        Set<Media> media = new HashSet<>(tagRepository.findById(id).media);
         tagRepository.findById(id).media.clear();
+        media.forEach(m -> m.getTags().clear());
         tagRepository.flush();
         return Response.ok(tagRepository.deleteById(id)).build();
     }
