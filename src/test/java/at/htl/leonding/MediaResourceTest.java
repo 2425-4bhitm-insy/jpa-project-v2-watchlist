059105e481eaa30daf8abe4a1f02cb9a91dbe3b8
FlorianHayer <f.hayer@students.htl-leonding.ac.at>
2025-04-29 09:25:13 +0200

    fix: tests fixed

--- src/test/java/at/htl/leonding/MediaResourceTest.java
+++ src/test/java/at/htl/leonding/MediaResourceTest.java
@@ -16,7 +16,7 @@ public class MediaResourceTest {
                .get("/media/0")
                .then()
                .statusCode(200)
-               .body(is("[{\"duration\":120,\"releaseDate\":[2023,1,1],\"rating\":5,\"edition\":1,\"name\":\"Movie 1\",\"description\":\"Description for Movie 1\",\"mediaType\":\"MOVIE\"},{\"duration\":45,\"releaseDate\":[2023,2,1],\"rating\":4,\"edition\":1,\"name\":\"Series 1\",\"description\":\"Description for Series 1\",\"mediaType\":\"SERIES\"},{\"duration\":60,\"releaseDate\":[2023,3,1],\"rating\":3,\"edition\":1,\"name\":\"Episode 1\",\"description\":\"Description for Episode 1\",\"mediaType\":\"EPISODE\"},{\"duration\":90,\"releaseDate\":[2023,1,1],\"rating\":5,\"edition\":1,\"name\":\"Series\",\"description\":\"Series all episodes\",\"mediaType\":\"SERIES\"},{\"duration\":90,\"releaseDate\":[2023,1,1],\"rating\":5,\"edition\":1,\"name\":\"Episode ONE\",\"description\":\"Series episode 1\",\"mediaType\":\"EPISODE\"},{\"duration\":90,\"releaseDate\":[2023,1,1],\"rating\":5,\"edition\":1,\"name\":\"Episode TWO\",\"description\":\"Series episode 1\",\"mediaType\":\"EPISODE\"},{\"duration\":90,\"releaseDate\":[2023,1,1],\"rating\":5,\"edition\":1,\"name\":\"Episode THREE\",\"description\":\"Series episode 1\",\"mediaType\":\"EPISODE\"},{\"duration\":90,\"releaseDate\":[2023,1,1],\"rating\":5,\"edition\":1,\"name\":\"Episode FOUR\",\"description\":\"Series episode 1\",\"mediaType\":\"EPISODE\"}]"));
+               .body(is("[{\"duration\":120,\"releaseDate\":\"2023-01-01\",\"rating\":5,\"edition\":1,\"name\":\"Movie 1\",\"description\":\"Description for Movie 1\",\"mediaType\":\"MOVIE\",\"tags\":[{\"tagId\":1,\"name\":\"Action\"}]},{\"duration\":45,\"releaseDate\":\"2023-02-01\",\"rating\":4,\"edition\":1,\"name\":\"Series 1\",\"description\":\"Description for Series 1\",\"mediaType\":\"SERIES\",\"tags\":[{\"tagId\":51,\"name\":\"Drama\"}]},{\"duration\":60,\"releaseDate\":\"2023-03-01\",\"rating\":3,\"edition\":1,\"name\":\"Episode 1\",\"description\":\"Description for Episode 1\",\"mediaType\":\"EPISODE\",\"tags\":[{\"tagId\":101,\"name\":\"Comedy\"}]},{\"duration\":90,\"releaseDate\":\"2023-01-01\",\"rating\":5,\"edition\":1,\"name\":\"Series\",\"description\":\"Series all episodes\",\"mediaType\":\"SERIES\",\"tags\":[]},{\"duration\":90,\"releaseDate\":\"2023-01-01\",\"rating\":5,\"edition\":1,\"name\":\"Episode ONE\",\"description\":\"Series episode 1\",\"mediaType\":\"EPISODE\",\"tags\":[]},{\"duration\":90,\"releaseDate\":\"2023-01-01\",\"rating\":5,\"edition\":1,\"name\":\"Episode TWO\",\"description\":\"Series episode 1\",\"mediaType\":\"EPISODE\",\"tags\":[]},{\"duration\":90,\"releaseDate\":\"2023-01-01\",\"rating\":5,\"edition\":1,\"name\":\"Episode THREE\",\"description\":\"Series episode 1\",\"mediaType\":\"EPISODE\",\"tags\":[]},{\"duration\":90,\"releaseDate\":\"2023-01-01\",\"rating\":5,\"edition\":1,\"name\":\"Episode FOUR\",\"description\":\"Series episode 1\",\"mediaType\":\"EPISODE\",\"tags\":[]}]"));
     }
 
     @Test
@@ -25,18 +25,27 @@ public class MediaResourceTest {
                .get("/media/1")
                .then()
                .statusCode(200)
-               .body(is("{\"duration\":120,\"releaseDate\":[2023,1,1],\"rating\":5,\"edition\":1,\"name\":\"Movie 1\",\"description\":\"Description for Movie 1\",\"mediaType\":\"MOVIE\"}"));
+               .body(is("{\"duration\":120,\"releaseDate\":\"2023-01-01\",\"rating\":5,\"edition\":1,\"name\":\"Movie 1\",\"description\":\"Description for Movie 1\",\"mediaType\":\"MOVIE\",\"tags\":[{\"tagId\":1,\"name\":\"Action\"}]}"));
+    }
+
+    @Test
+    public void test_get_average_rating_by_person() {
+        given().when()
+               .get("/media/averageRating/1")
+               .then()
+               .statusCode(200)
+               .body(is("4.5"));
     }
 
     @Test
     public void test_create() {
         given().when()
                .contentType("application/json")
-               .body("{\"duration\":0,\"releaseDate\":null,\"rating\":0,\"edition\":0,\"name\":\"Action\",\"description\":null,\"mediaType\":null}")
+               .body("{\"duration\":0,\"releaseDate\":null,\"rating\":0,\"edition\":0,\"name\":\"Action\",\"description\":null,\"mediaType\":null,\"tags\":[]}")
                .post("/media")
                .then()
                .statusCode(200)
-               .body(is("{\"duration\":0,\"releaseDate\":null,\"rating\":0,\"edition\":0,\"name\":\"Action\",\"description\":null,\"mediaType\":null}"));
+               .body(is("{\"duration\":0,\"releaseDate\":null,\"rating\":0,\"edition\":0,\"name\":\"Action\",\"description\":null,\"mediaType\":null,\"tags\":[]}"));
     }
 
     @Test
@@ -61,15 +70,6 @@ public class MediaResourceTest {
                .then()
                .statusCode(200)
                .body(is("[{\"personId\":1,\"firstName\":\"John\",\"lastName\":\"Doe\",\"email\":\"john.doe@example.com\"},{\"personId\":51,\"firstName\":\"Jane\",\"lastName\":\"Smith\",\"email\":\"jane.smith@example.com\"},{\"personId\":101,\"firstName\":\"Alice\",\"lastName\":\"Johnson\",\"email\":\"alice.johnson@example.com\"}]"));
-    }
-
-    @Test
-    public void test_get_average_rating_by_person() {
-        given().when()
-               .get("/media/averageRating/1")
-               .then()
-               .statusCode(200)
-               .body(is("4.0"));
     }
 
 }
--- src/test/java/at/htl/leonding/PersonResourceTest.java
+++ src/test/java/at/htl/leonding/PersonResourceTest.java
@@ -1,5 +1,6 @@
 package at.htl.leonding;
 
+import at.htl.leonding.entities.PersonType;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
@@ -11,17 +12,52 @@ public class PersonResourceTest {
     @Test
     public void test_get_all() {
         given().when()
-                .get("/person/1")
+                .get("/person/0")
                 .then()
                 .statusCode(200)
-                .body(is("{\"personId\":1,\"firstName\":\"John\",\"lastName\":\"Doe\",\"email\":\"john.doe@example.com\"}"));
+                .body(is("[{\"personId\":1,\"firstName\":\"John\",\"lastName\":\"Doe\",\"email\":\"john.doe@example.com\"},{\"personId\":51,\"firstName\":\"Jane\",\"lastName\":\"Smith\",\"email\":\"jane.smith@example.com\"},{\"personId\":101,\"firstName\":\"Alice\",\"lastName\":\"Johnson\",\"email\":\"alice.johnson@example.com\"}]"));
     }
     @Test
     public void test_create() {
         given().when()
-                .get("/media/1")
-                .then()
-                .statusCode(200)
-                .body(is(""));
+               .contentType("application/json")
+               .body("{\"duration\":0,\"releaseDate\":null,\"rating\":0,\"edition\":0,\"name\":\"Action\",\"description\":null,\"mediaType\":null,\"tags\":[]}")
+               .post("/person")
+               .then()
+               .statusCode(200)
+               .body(is("{\"personId\":151,\"firstName\":null,\"lastName\":null,\"email\":null}"));
+    }
+
+    @Test
+    public void test_get_by_id() {
+        given().when()
+               .get("/person/1")
+               .then()
+               .statusCode(200)
+               .body(is("{\"personId\":1,\"firstName\":\"John\",\"lastName\":\"Doe\",\"email\":\"john.doe@example.com\"}"));
+    }
+
+    @Test
+    public void test_delete() {
+        given().when()
+               .delete("/person/101")
+               .then()
+               .statusCode(200)
+               .body(is("true"));
+
+        given().when()
+               .get("/person/101")
+               .then()
+               .statusCode(200)
+               .body(is(""));
+    }
+
+    @Test
+    public  void test_get_most_relevant_actor() {
+        given().when()
+               .get("/person/mostRelevant/" + PersonType.actors)
+               .then()
+               .statusCode(200)
+               .body(is("{\"personId\":101,\"firstName\":\"Alice\",\"lastName\":\"Johnson\",\"email\":\"alice.johnson@example.com\"}"));
     }
 }
